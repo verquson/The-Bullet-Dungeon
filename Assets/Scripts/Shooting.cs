e8525a3c8e1f8ca5694d7ad73eb18f512ec79982
BirkBagge <81672464+BirkBagge@users.noreply.github.com>
2022-08-31 12:50:30 +0300

    shooting.1.1

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -18,4 +18,13 @@ public class Bullets : MonoBehaviour
     {
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
+
+    public float thrust = 1.0f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+    }
 }
