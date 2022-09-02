69a05a82dcbd8c09bd9fcc67f6423a1b69e0c115
BirkBagge <81672464+BirkBagge@users.noreply.github.com>
2022-09-02 14:19:28 +0300

    Heals1

--- Assets/Scripts/Heals.cs
+++ Assets/Scripts/Heals.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class Heals : MonoBehaviour
+    
+{  
+    public int Healing;
+
+
+// Start is called before the first frame update
+void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    public void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.GetComponent<HealthAndArmor>())
+        {
+            collision.gameObject.GetComponent<HealthAndArmor>().AddHealth();
+        }
     }
+
+
+
 }
